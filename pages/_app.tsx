d36f1d0607fa04f5ba89c23c9eb63dcf1754ca56
Ryan <ryan@mwire.dev>
2024-10-24 12:01:09 +0900

    설명 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,15 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
     >
       <Head>
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://lazysoul.com/" />
+        <meta property="og:title" content="I value the beauty of taking things slowly in development." />
+        <meta property="og:description" content="I enjoy learning new technologies and methodologies, and I'd like to share the insights and experiences gained through various experiments with other developers. Let me share my journey as a developer who grows steadily, albeit slowly." />
+        <meta property="og:image" content="https://lazysoul.com/lazysoul-tortoise.png" />
+
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
