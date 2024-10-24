424d7c6ee798bd85d20281c8263738945ca6c8d8
Ryan <ryan@mwire.dev>
2024-10-24 11:41:24 +0900

    설명 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,9 +15,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
-          content="Clone and deploy your own Next.js portfolio in minutes."
+          content="I enjoy learning new technologies and methodologies, and I'd like to share the insights and experiences gained through various experiments with other developers. Let me share my journey as a developer who grows steadily, albeit slowly."
         />
-        <title>My awesome blog</title>
+        <title>I value the beauty of taking things slowly in development.</title>
       </Head>
 
       <Header />
